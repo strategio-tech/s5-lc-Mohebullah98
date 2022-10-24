6284111053272b1d0e9caa7c28d5a95d1859170c
Mohebullah Mir <mohebullahmir@gmail.com>
2022-10-24 13:15:14 -0400

    Finsihed method

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -7,12 +7,20 @@ public class LoanCalculator {
     /**
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     * Takes amount in as input and and iterates 3 times for amount of months
+     * through each iteration amount is decreased by 10%.
+     * Return amount when done.
+     * @param /Method takes amount in as input,
+     * @return Returns int amount when finished.
      */
     static int getRemainingAmountIn3Months(int amount) {
         // TODO: Add your code below
+        int count=0;
+        while(count<3){
+            amount-=amount/10;
+            count++;
+        }
+        return amount;
     }
 
     public static void main(String[] args) {
